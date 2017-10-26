11f5edd2e76b5b8f3b546c8f084b30aeae7e8a86
Brooke Lampe <belampe13@gmail.com>
2017-10-26 16:51:08 -0500

    Fixed repeated line in unit_tests.html causing Script Error.

--- unit_tests/test_priority_queue.js
+++ unit_tests/test_priority_queue.js
@@ -1,33 +1,61 @@
 QUnit.module('priority_queue.js');
 /* globals QUnit PriorityQueue */
-/* eslint-disable no-magic-numbers */
+/* eslint-disable no-magic-numbers, no-underscore-dangle */
 
 /*  Method
 
-    _getNextIndex
-    Categories
-      Number of elements
-        No elements
-        One elements
-        More than one element
-      Priorities
-        Same Priorities
-        Different Priorities
+      Categories (same categories _getNextIndex, peek, dequeue)
+        Number of elements
+          No element in queue
+          One element in queue
+          More than one element in queue
+        Priorities
+          Same priorities for multiple elements in queue
+          Different for all elements in queue
 
-      peek
+      Categories (enqueue)
+        Existence
+          Element to be added exists in queue
+          Element to be added does not exist in queue
+        Priorities
+          Element to be added has same priority as an element in queue
+          Element to be added does not have same priority as an element in queue
+
+      Categories (enqueue)
+        Existence
+          Element to be deleted exists in queue
+          Element to be deleted does not exist in queue
 
 */
 
-//  _getNextIndex
-//  Categories
-//
+function metric(element) {
+  let value = undefined;
 
-//  enqueue
+  if (element < 0) {
+    value = -1;
+  } else if (element === 0) {
+    value = 0;
+  } else if (element < 2) {
+    value = 1;
+  } else if (element < 10) {
+    value = 2;
+  } else if (element < 20) {
+    value = 3;
+  } else {
+    value = 4;
+  }
 
-//  dequeue
+  return value;
+}
 
-//  delete
+QUnit.test('Get the next index of a queue with no element', (assert) => {
+  const pQueue = new PriorityQueue(metric, []);
+  const next = pQueue._getNextIndex();
+  assert.deepEqual(next, undefined);
+});
 
-QUnit.test('do', (assert) => {
-  assert.deepEqual(2, 2);
+QUnit.test('Get the next index of a queue with one element', (assert) => {
+  const pQueue = new PriorityQueue(metric, [5]);
+  const next = pQueue._getNextIndex();
+  assert.deepEqual(next, '5');
 });
